29233caf35d3b9a49174f3e5df397d39dfbca195
Luana Portela <enidc2003@gmail.com>
2025-06-25 18:44:43 -0300

    Atualizacão

--- src/assets/componentes/ComponenteProps.jsx
+++ src/assets/componentes/ComponenteProps.jsx
@@ -1,11 +1,11 @@
-import styles from '../css/ComponenteProps.module.css'
+import Styles from '../css/ComponenteProps.module.css'
 
 function ComponenteProps({ lugar, texto, logo_texto }) {
   return (
-    <div className={styles.corpo_texto}>
+    <div className={Styles.corpo_texto}>
       <h2>Venha Explorar esse lugar incrível {lugar}!!</h2>
       <p>{texto}</p>
-      <img src={logo_texto} alt="logo" className={styles.icoViagem} />
+      <img src={logo_texto} alt="logo" className={Styles.icoViagem} />
     </div>
   )
 }
